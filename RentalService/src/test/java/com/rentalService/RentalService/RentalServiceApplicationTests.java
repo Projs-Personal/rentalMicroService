6c292af775135154a8dc1720fe173ce1f0fa1906
Anshika Priyadarshini <144573448+P7Anshika@users.noreply.github.com>
2024-11-11 23:57:02 +0530

    Add files via upload

---
+++ RentalService/src/test/java/com/rentalService/RentalService/RentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentalService.RentalService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
